9e1fef22f7469abf4dd949946dd27091f0119ce5
Luis Chang <luis.chang@outlook.com>
2021-11-22 22:35:41 -0500

    Agregar archivos de proyecto.

---
+++ M2NETCore_HelloMVC.Web/Controllers/SecurityController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M2NETCore_HelloMVC.Web.Controllers
+{
+    public class SecurityController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //public IActionResult Login(string email, string password)
+        public IActionResult Login(IFormCollection form)
+        {
+            var email = form["email"].ToString();
+            var password = form["password"].ToString();
+
+            if (email == "luis@qbo.com" && password == "123456")
+            {
+                return RedirectToAction("Index","Home");
+
+            }
+
+            return RedirectToAction("Login");
+        }
+    }
+}
